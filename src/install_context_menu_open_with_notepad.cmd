fb09547376e990bee2e78289abaa5d07b2fb88f7
Clemens Wacha <clemens@wacha.ch>
2019-08-31 08:54:45 +0200

    Added support to install context menu for all users when run as admin

--- src/install_context_menu_open_with_notepad.cmd
+++ src/install_context_menu_open_with_notepad.cmd
@@ -1,10 +1,19 @@
 @echo off
 
 set BASEDIR=%~dp0
-if "%BASEDIR:~-1" == "\" set BASEDIR=%BASEDIR:~0,-1%
+if "%BASEDIR:~-1%" == "\" set BASEDIR=%BASEDIR:~0,-1%
 
 set action=Edit with Notepad++
 set command=%BASEDIR%\notepad++.exe
 
-reg.exe add "HKEY_CURRENT_USER\Software\Classes\*\shell\%action%\command" /ve /t REG_EXPAND_SZ /d "\"%command%\"\"%%1\"" /f
-reg.exe add "HKEY_CURRENT_USER\Software\Classes\*\shell\%action%" /v Icon /t REG_SZ /d "%command%,0" /f
+
+:: Detect if we have Administrator rights
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	set hkey=HKEY_LOCAL_MACHINE
+) else (
+	set hkey=HKEY_CURRENT_USER
+)
+
+reg.exe add "%hkey%\Software\Classes\*\shell\%action%\command" /ve /t REG_EXPAND_SZ /d "\"%command%\"\"%%1\"" /f
+reg.exe add "%hkey%\Software\Classes\*\shell\%action%" /v Icon /t REG_SZ /d "%command%,0" /f
