191f660fdf76e45742becf344dd152f3975ddeb2
Clemens Wacha <clemens@wacha.ch>
2019-10-02 23:28:57 +0200

    enabled use of notepad when installed with a choco installation in C:\ProgramData (where only admins have write permissions)

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -1,3 +1,13 @@
-﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿function isAdmin() {
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+	$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 & $toolsDir\root\install_context_menu_open_with_notepad.cmd
 Install-BinFile -Name npp-menu -Path $toolsDir\root\install_context_menu_open_with_notepad.cmd
+
+# ensure configs are saved to users AppData folder to enable use for non-admin users
+if(isAdmin) {
+	rm $toolsDir\root\doLocalConf.xml
+}
