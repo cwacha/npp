35d5e8bcd97503de344804ee595a4b2649db629e
Clemens Wacha <clemens@wacha.ch>
2019-10-03 00:09:01 +0200

    added steps to remove start menu and shell action on uninstall

--- pkg/nupkg/tools/chocolateyuninstall.ps1
+++ pkg/nupkg/tools/chocolateyuninstall.ps1
@@ -1,2 +1,15 @@
-﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿function isAdmin() {
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+	$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # & $toolsDir\control.cmd remove
+
+if(isAdmin) {
+	"Removing Right-click shell action"
+	Remove-Item -Recurse -LiteralPath 'HKLM:\Software\Classes\*\shell\Edit with Notepad++'
+
+	"Removing Start Menu entry"
+	rm -fo -ea SilentlyContinue "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Notepad++.lnk"
+}
