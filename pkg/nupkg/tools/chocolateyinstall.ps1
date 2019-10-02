f50680c29e0ce8cece2c027f9a9024ad3e453009
Clemens Wacha <clemens@wacha.ch>
2019-10-02 23:49:07 +0200

    added start menu entry when installed with admin rights

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -7,7 +7,15 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 & $toolsDir\root\install_context_menu_open_with_notepad.cmd
 Install-BinFile -Name npp-menu -Path $toolsDir\root\install_context_menu_open_with_notepad.cmd
 
-# ensure configs are saved to users AppData folder to enable use for non-admin users
+
 if(isAdmin) {
+	# ensure configs are saved to users AppData folder to enable use for non-admin users
+	"Removing doLocalConf.xml"
 	rm $toolsDir\root\doLocalConf.xml
+	
+	# create start menu entry
+	"Creating Start Menu entry"
+	Install-ChocolateyShortcut `
+		-ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Notepad++.lnk"`
+		-TargetPath "$toolsDir\root\notepad++.exe"
 }
