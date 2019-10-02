1348f92ba74ddbb1a2dda6b4754f985d45d9e84c
Clemens Wacha <clemens.wacha@ubs.com>
2019-10-02 21:37:36 +0200

    added shim npp-menu to re-install context menu settings

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -1,2 +1,3 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 & $toolsDir\root\install_context_menu_open_with_notepad.cmd
+Install-BinFile -Name npp-menu -Path $toolsDir\root\install_context_menu_open_with_notepad.cmd
