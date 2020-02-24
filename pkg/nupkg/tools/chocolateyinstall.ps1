772e40c664150add85ef4c0bd2a135bf6cb465f0
Clemens Wacha <clemens.wacha@ubs.com>
2020-02-24 12:05:36 +0100

    cosmetic changes

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿function isAdmin() {
-	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-	$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
@@ -10,14 +10,14 @@ $startmenudir = "$env:AppData\Microsoft\Windows\Start Menu\Programs"
 Install-BinFile -Name npp-menu -Path $toolsDir\root\install_context_menu_open_with_notepad.cmd
 
 if(isAdmin) {
-	# ensure configs are saved to users AppData folder to enable use for non-admin users
-	"Removing doLocalConf.xml"
-	rm $toolsDir\root\doLocalConf.xml
-	
-	$startmenudir = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs"
+    # ensure configs are saved to users AppData folder to enable use for non-admin users
+    "Removing doLocalConf.xml"
+    rm $toolsDir\root\doLocalConf.xml
+    
+    $startmenudir = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs"
 }
 
 "Creating Start Menu entry"
 Install-ChocolateyShortcut `
-	-ShortcutFilePath "$startmenudir\Notepad++.lnk"`
-	-TargetPath "$toolsDir\root\notepad++.exe"
+    -ShortcutFilePath "$startmenudir\Notepad++.lnk"`
+    -TargetPath "$toolsDir\root\notepad++.exe"
--- pkg/pkgmake.ps1
+++ pkg/pkgmake.ps1
@@ -1,30 +1,31 @@
 param (
-	[string]$rule = ""
+    [string]$rule = ""
 )
 
 $BASEDIR=$PSScriptRoot
 #echo BASEDIR=$BASEDIR
 
 function all {
-	clean
-	import
-	pkg
-	nupkg
+    "# building: $app_pkgname"
+    clean
+    import
+    pkg
+    nupkg
 }
 
 function _init {
-	$global:app_pkgid = "npp"
-	$global:app_version = Get-ChildItem $BASEDIR\..\ext\*.zip | %{$_.Name -replace "npp.", "" -replace ".zip", "" -replace ".bin.*", ""}
+    $global:app_pkgid = "npp"
+    $global:app_displayname = "Notepad++"
+    $global:app_version = Get-ChildItem $BASEDIR\..\ext\*.zip | %{$_.Name -replace "npp.", "" -replace ".zip", "" -replace ".bin.*", ""}
     $global:app_revision = (git describe --tags --abbrev=0 | %{git log "$_..HEAD" --oneline}).count
     $global:app_build = git rev-parse --short HEAD
 
-	$global:app_pkgname = "$app_pkgid-$app_version-$app_revision-$app_build"
+    $global:app_pkgname = "$app_pkgid-$app_version-$app_revision-$app_build"
 }
 
 function _template {
     param (
-        [string] $inputfile,
-        [string] $outputfile
+        [string] $inputfile
     )
     Get-Content $inputfile | %{ $_ `
         -replace "%app_pkgid%", "$app_pkgid" `
@@ -36,24 +37,24 @@ function _template {
 }
 
 function import {
-	"# import ..."
-	mkdir BUILD/root -ea SilentlyContinue *> $null
-	
-	Expand-Archive -Path ..\ext\*.zip -DestinationPath BUILD/root
-	cp -r -fo ..\src\* BUILD/root
+    "# import ..."
+    mkdir BUILD/root -ea SilentlyContinue *> $null
+    
+    Expand-Archive -Path ..\ext\*.zip -DestinationPath BUILD/root
+    cp -r -fo ..\src\* BUILD/root
     cp BUILD/root/stylers.model.xml BUILD/root/themes/default.xml
     cp BUILD/root/themes/Monokai_2.xml BUILD/root/stylers.model.xml
-	rm BUILD/root/config.xml
+    rm BUILD/root/config.xml
 }
 
 function pkg {
-	"# packaging ..."
-	mkdir PKG > $null
-	
-	cd BUILD
-	Compress-Archive -Path root\* -DestinationPath ..\PKG\$app_pkgname.zip
-	cd ..
-	"## created $BASEDIR\PKG\$app_pkgname.zip"
+    "# packaging ..."
+    mkdir PKG > $null
+    
+    cd BUILD
+    Compress-Archive -Path root\* -DestinationPath ..\PKG\$app_pkgname.zip
+    cd ..
+    "## created $BASEDIR\PKG\$app_pkgname.zip"
 }
 
 function nupkg {
@@ -74,18 +75,18 @@ function nupkg {
 }
 
 function clean {
-	"# clean ..."
-	rm -r -fo -ea SilentlyContinue PKG
-	rm -r -fo -ea SilentlyContinue BUILD
+    "# clean ..."
+    rm -r -fo -ea SilentlyContinue PKG
+    rm -r -fo -ea SilentlyContinue BUILD
 }
 
 $funcs = Select-String -Path $MyInvocation.MyCommand.Path -Pattern "^function ([^_]\S+) " | %{$_.Matches.Groups[1].Value}
 if(! $funcs.contains($rule)) {
-	"no such rule: '$rule'"
-	""
-	"RULES"
-	$funcs | %{"    $_"}
-	exit 1
+    "no such rule: '$rule'"
+    ""
+    "RULES"
+    $funcs | %{"    $_"}
+    exit 1
 }
 
 cd "$BASEDIR"
